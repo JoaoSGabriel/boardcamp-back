58da208c41142e5c9553fb693ab3209ed2e57588
JoaoSGabriel <joaogabriel0359@gmail.com>
2022-09-30 08:46:25 -0300

    implement CRUD on categoriesController

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -10,12 +10,24 @@ export async function list(req, res) {
 }
 
 export async function create(req, res) {
-  const categorie = req.body;
+  const categorie = req.body.name;
+  console.log(categorie);
+
+  if (!categorie) return res.sendStatus(400);
+
   try {
-    await connection.query("INSERT INTO categories (nome) VALUES $1;", [
-      categorie,
-    ]);
-    res.sendStatus(201);
+    const exist = await connection.query(
+      "SELECT * FROM categories WHERE name = $1;",
+      [categorie]
+    );
+    if (exist.rows.length === 0) {
+      await connection.query("INSERT INTO categories (name) VALUES ($1);", [
+        categorie,
+      ]);
+      return res.sendStatus(201);
+    } else {
+      return res.sendStatus(409);
+    }
   } catch (error) {
     return res.status(500).send(error.message);
   }
